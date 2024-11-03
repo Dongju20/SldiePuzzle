c218a9c8cf1813db391b839fcd507e6caff08252
Kim-WanKyu <116157873+Kim-WanKyu@users.noreply.github.com>
2024-11-04 00:45:05 +0900

    241103 오브젝트 닫기 버튼 기능 구현
    ObjectExitButton.cs 수정. 오브젝트 닫기 버튼 기능 구현. (퍼즐 오브젝트 비활성화).
    
    + Preset1, 2, 3 prefab 에 닫기 버튼 상호작용 시 닫을 퍼즐 오브젝트 넣음.

--- Assets/Script/UI/ObjectExitButton.cs
+++ Assets/Script/UI/ObjectExitButton.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 // 에셋에서 클릭 가능한 나가기 버튼 오브젝트 클래스.
 public class ObjectExitButton : ObjectButton
 {
+    [SerializeField]
+    private GameObject puzzleObject;
+
     public override void ClickButton()
     {
         Exit();
     }
 
     public void Exit() {
-        // TODO gameObject 대신, 슬라이딩 퍼즐 보드 오브젝트를 가져올 수 있어야 함.
-        gameObject.SetActive(false);
+        puzzleObject.SetActive(false);
     }
 }

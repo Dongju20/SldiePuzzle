9429aa13c920cac30ecbf0c74f79a784ea5f6744
Kim-WanKyu <116157873+Kim-WanKyu@users.noreply.github.com>
2024-11-04 01:52:50 +0900

    241103 오브젝트 섞기 버튼 기능 구현
    ObjectShuffleButton.cs 수정. 오브젝트 섞기 버튼 기능 구현.
    
    Preset2.prefab 에 오브젝트 섞기 버튼 추가.

--- Assets/Script/UI/ObjectShuffleButton.cs
+++ Assets/Script/UI/ObjectShuffleButton.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 // 에셋에서 클릭 가능한 섞기 버튼 오브젝트 클래스.
 public class ObjectShuffleButton : ObjectButton
 {
+    [SerializeField]
+    private Board puzzleBoard;
+
     public override void ClickButton()
     {
         Shuffle();
     }
 
     public void Shuffle() {
-        Debug.Log("Shuffle");
+        StartCoroutine(puzzleBoard.Shuffle());
     }
 }

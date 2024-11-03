5dbc561899a977ac641725eb582ecaf708e4127f
Kim-WanKyu <116157873+Kim-WanKyu@users.noreply.github.com>
2024-11-04 05:55:03 +0900

    241103 SlidingPuzzle 폴더 추가 후 모든 파일 및 폴더 집어넣음.
    241103 SlidingPuzzle 폴더 추가 후 모든 파일 및 폴더 집어넣음.

---
+++ Assets/SlidingPuzzle/Editor/ImageEditor.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ImageController))]
+public class ImageEditor : Editor
+{
+    public ImageController imageController;
+    
+    private void OnEnable()
+    {
+        if(AssetDatabase.Contains(target)) {
+            imageController = null;
+            Debug.Log("여기 실행..");
+        }
+        else
+        {
+            // target은 Object형이므로 ImageController 형변환
+            imageController = (ImageController)target;
+            Debug.Log("타켓 실행");
+        }
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        if(imageController == null)
+        {
+            return;
+        }
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Select Type (Texture and Sprite only)");
+        EditorGUILayout.Space(10);
+        imageController.imageType = (ImageType)EditorGUILayout.EnumPopup("ImageType", imageController.imageType);
+        switch(imageController.imageType)
+        {
+            case ImageType.Sprite:
+                imageController.sprite = (Sprite)EditorGUILayout.ObjectField("Sprite", imageController.sprite, typeof(Sprite), true);
+                break;
+            case ImageType.Texture2D:
+                imageController.texture = (Texture)EditorGUILayout.ObjectField("Texture", imageController.texture, typeof(Texture), true);
+                break;
+
+        }
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Select RenderMode (Opaque and fade only)");
+        EditorGUILayout.Space(10);
+        imageController.renderType = (RenderType)EditorGUILayout.EnumPopup("RederType", imageController.renderType);
+        switch (imageController.renderType)
+        {
+            //투명한 경우..
+            case RenderType.Transparent:
+                imageController.alpha = EditorGUILayout.IntSlider("Alpha", imageController.alpha, 0, 255);
+                break;
+        }
+        EditorUtility.SetDirty(imageController);
+    }
+}

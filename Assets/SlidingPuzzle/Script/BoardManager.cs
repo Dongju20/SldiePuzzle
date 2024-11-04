83f2af5628c4f76f0f056515fdad7d280d2b53c0
Kim-WanKyu <116157873+Kim-WanKyu@users.noreply.github.com>
2024-11-04 18:42:51 +0900

    241104 에러 수정.
    BoardManager.cs 불필요 using 문 제거
    IClickableObj.cs 불필요 한정자 public 제거
    
    태그 매니저 프리셋 추가.

--- Assets/SlidingPuzzle/Script/BoardManager.cs
+++ Assets/SlidingPuzzle/Script/BoardManager.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
 
 [RequireComponent(typeof(Camera))]
 public class BoardManager : MonoBehaviour
--- Assets/SlidingPuzzle/Script/UI/IClickableObj.cs
+++ Assets/SlidingPuzzle/Script/UI/IClickableObj.cs
@@ -1,5 +1,5 @@
 // 에셋에서 클릭 가능한 오브젝트에 들어갈 인터페이스.
 public interface IClickableObj
 {
-    public void ClickObj(); // 오브젝트 클릭 시 조작 메소드.
+    void ClickObj(); // 오브젝트 클릭 시 조작 메소드.
 }
